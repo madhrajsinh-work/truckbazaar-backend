10ecf07df0b6a3c71d14e9bdb1815d7a51b08ad0
Rushi <madhrajsinh.work@gmail.com>
2025-04-17 10:09:55 +0530

    Update db.js

--- db/db.js
+++ db/db.js
@@ -2,9 +2,8 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
   try {
-    const safeURI = process.env.MONGO_URI.replace(/\/\/.*?:.*?@/, '//<user>:<pass>@');
     await mongoose.connect(process.env.MONGO_URI);
-    console.log(`${safeURI}`);
+    console.log('DB Connected');
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
     process.exit(1);
